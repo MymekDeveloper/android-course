bc0725cb25423cc16aa147efc4cec7522540425e
MymekDeveloper <szwed.szymon1@gmail.com>
2021-11-22 18:32:31 +0100

    Repaired Text types and added poping up notifications

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
+
+        Toast.makeText(this, "wow, I can make an alert pop up",Toast.LENGTH_LONG).show();
     }
 }
